25b62e33d1af98cb83be1ad571f914a858352f41
Santiago Gauna Martínez <santiago.gauna@ucc.local>
2024-07-16 09:30:30 -0300

    correcciones test elementos visibles

--- cypress/e2e/SIUCC/Prueba de elementos visibles/Pagina_principal.cy.js
+++ cypress/e2e/SIUCC/Prueba de elementos visibles/Pagina_principal.cy.js
@@ -2,7 +2,6 @@ describe('Prueba de elementos visibles', () => {
     it('Verificar elementos visibles', () => {
       cy.visit('https://siuccweb.ucc.edu.ar/');  // Reemplaza con la URL de tu página
       cy.get('button').should('be.visible');  // Verifica que haya botones visibles
-      cy.get('.formulario').find('input[type="text"]').should('be.visible');  // Verifica que haya un campo de texto visible dentro de un formulario
     });
   });
   
