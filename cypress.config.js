f8e8ca2ba835a728d20b60ca9305ebe8e0ec7129
Santiago Gauna Martínez <santiago.gauna@ucc.local>
2024-07-19 09:37:19 -0300

    Cambios 19/07/2024

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '4xw7rg',
   chromeWebSecurity: false, // Deshabilita la seguridad web para permitir contenido mixto
   e2e: {
     setupNodeEvents(on, config) {
