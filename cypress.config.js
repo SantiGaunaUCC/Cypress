5a3ab4217b4f74f73c7f8c09a5b83048a6e11c78
Santiago Gauna Martínez <santiago.gauna@ucc.local>
2024-07-22 13:00:46 -0300

    Desconexión con Cypress Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '4xw7rg',
-  apiKey: '33b699b2-2219-462b-9435-5d04260885e2',
+  // projectId: '4xw7rg',
+  // apiKey: '33b699b2-2219-462b-9435-5d04260885e2',
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
